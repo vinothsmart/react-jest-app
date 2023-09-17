2cafa9431460dcb61de9df0219a46add76056463
vinothsmart <s.vinothkanna90@gmail.com>
2023-09-17 11:51:11 +0530

    other functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import {
+  getByAltText,
+  getByDisplayValue,
+  getByLabelText,
+  getByPlaceholderText,
+  getByRole,
+  getByTitle,
+  render,
+  screen,
+} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,4 +16,11 @@ test('renders learn react link', () => {
   expect(linkElement).not.toBeInTheDocument();
   const userName = screen.queryByTestId('user-name');
   expect(userName).not.toBeInTheDocument();
+  expect(userName).toBeInTheDocument();
+  // getByRole(userName, 'heading'); - this is used for heading
+  // getByPlaceholderText(userName, 'VinothKanna'); - this is used for placeholder text
+  // getByLabelText(userName, 'VinothKanna'); - this is used for label text
+  // getByAltText(userName, 'VinothKanna'); - this is used for image alt text
+  // getByDisplayValue(userName, 'VinothKanna'); // this is used for input value
+  // getByTitle(userName, 'VinothKanna'); // this is used for title
 });
